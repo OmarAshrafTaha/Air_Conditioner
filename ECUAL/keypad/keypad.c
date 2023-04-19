bdbd213d4c16bb707602f320b85d86456b03a6b6
Omar Taha <omarashraftaha19@gmail.com>
2023-04-19 20:52:45 +0200

    Updated Keypad with Pull up configuration

--- ECUAL/keypad/keypad.c
+++ ECUAL/keypad/keypad.c
@@ -10,7 +10,6 @@
 #define ROWS_SIZE 4
 #define COLUMNS_SIZE 3
 
-
 /***************************************************************************/
 /** @brief  Keypad initialization                                          */
 /** @param  void                                                           */
@@ -19,32 +18,36 @@
 /***************************************************************************/
 KEYPAD_initError KEYPAD_init(void)
 {
-    /*Rows initialization*/
-    DIO_init(R1,ROW_PORT_DATA,OUTPUT);
-    DIO_init(R2,ROW_PORT_DATA,OUTPUT);
-    DIO_init(R3,ROW_PORT_DATA,OUTPUT);
-    DIO_init(R4,ROW_PORT_DATA,OUTPUT);
+    /*Rows direction initialization*/
+    DIO_init(R1, ROW_PORT_DATA, OUTPUT);
+    DIO_init(R2, ROW_PORT_DATA, OUTPUT);
+    DIO_init(R3, ROW_PORT_DATA, OUTPUT);
+    DIO_init(R4, ROW_PORT_DATA, OUTPUT);
 
-    DIO_write(R1,ROW_PORT_DATA,HIGH);
-    DIO_write(R2,ROW_PORT_DATA,HIGH);
-    DIO_write(R3,ROW_PORT_DATA,HIGH);
-    DIO_write(R4,ROW_PORT_DATA,HIGH);
+    /*Columns direction initialization*/
+    DIO_init(C1, COLUMN_PORT_DATA, INPUT);
+    DIO_init(C2, COLUMN_PORT_DATA, INPUT);
+    DIO_init(C3, COLUMN_PORT_DATA, INPUT);
 
-// 	DIO_write(R1,ROW_PORT_DATA,LOW);
-// 	DIO_write(R2,ROW_PORT_DATA,LOW);
-// 	DIO_write(R3,ROW_PORT_DATA,LOW);
-// 	DIO_write(R4,ROW_PORT_DATA,LOW);
-	
-    /*Columns initialization*/
-    DIO_init(C1,COLUMN_PORT_DATA,INPUT);
-    DIO_init(C2,COLUMN_PORT_DATA,INPUT);
-    DIO_init(C3,COLUMN_PORT_DATA,INPUT);
+/*Initial pin values*/
+    #if CONNECTION == 'D'
+    DIO_write(R1, ROW_PORT_DATA, LOW);
+    DIO_write(R2, ROW_PORT_DATA, LOW);
+    DIO_write(R3, ROW_PORT_DATA, LOW);
+    DIO_write(R4, ROW_PORT_DATA, LOW);
 
-    DIO_write(C1,COLUMN_PORT_DATA,HIGH);
-    DIO_write(C2,COLUMN_PORT_DATA,HIGH);
-    DIO_write(C3,COLUMN_PORT_DATA,HIGH);
+    #elif CONNECTION == 'U'
+    DIO_write(R1, ROW_PORT_DATA, HIGH);
+    DIO_write(R2, ROW_PORT_DATA, HIGH);
+    DIO_write(R3, ROW_PORT_DATA, HIGH);
+    DIO_write(R4, ROW_PORT_DATA, HIGH);
 
-	return KEYPAD_initSuccess;
+    DIO_write(C1, COLUMN_PORT_DATA, HIGH);
+    DIO_write(C2, COLUMN_PORT_DATA, HIGH);
+    DIO_write(C3, COLUMN_PORT_DATA, HIGH);
+
+    #endif
+    return KEYPAD_initSuccess;
 }
 
 /***************************************************************************/
@@ -55,30 +58,40 @@ KEYPAD_initError KEYPAD_init(void)
 /***************************************************************************/
 KEYPAD_readError KEYPAD_read(uint8_t *u8_a_value)
 {
-	uint8_t i = 0;
-	uint8_t j = 0;	
-    uint8_t u8_l_Rows[ROWS_SIZE] = {R1,R2,R3,R4};
-    uint8_t u8_l_Columns[COLUMNS_SIZE] = {C1,C2,C3};
+    uint8_t i = 0;
+    uint8_t j = 0;
+
+    #if CONNECTION == 'U'
+    #define u8_l_readCheck LOW
+    #define u8_l_pinValue LOW
+    #elif CONNECTION == 'D'
+    #define u8_l_readCheck HIGH
+    #define u8_l_pinValue HIGH
+    #endif
+
+    uint8_t u8_l_Rows[ROWS_SIZE] = {R1, R2, R3, R4};
+    uint8_t u8_l_Columns[COLUMNS_SIZE] = {C1, C2, C3};
+
     for (i = 0; i < ROWS_SIZE; i++)
     {
-        DIO_write(u8_l_Rows[i],ROW_PORT_DATA,LOW);
+        DIO_write(u8_l_Rows[i], ROW_PORT_DATA, u8_l_pinValue);
 
-        for(j = 0; j < COLUMNS_SIZE; j++)
+        for (j = 0; j < COLUMNS_SIZE; j++)
         {
-            DIO_read(u8_l_Columns[j],COLUMN_PORT_DATA,u8_a_value);
-            if (*u8_a_value == 0)
+            DIO_read(u8_l_Columns[j], COLUMN_PORT_DATA, u8_a_value);
+            if (*u8_a_value == u8_l_readCheck)
             {
                 if (i == 0)
                 {
-                    if(j == 0)
+                    if (j == 0)
                     {
                         *u8_a_value = '1';
                     }
-                    else if(j==1)
+                    else if (j == 1)
                     {
                         *u8_a_value = '2';
                     }
-                    else if(j==2)
+                    else if (j == 2)
                     {
                         *u8_a_value = '3';
                     }
@@ -86,15 +99,15 @@ KEYPAD_readError KEYPAD_read(uint8_t *u8_a_value)
 
                 else if (i == 1)
                 {
-                    if(j == 0)
+                    if (j == 0)
                     {
                         *u8_a_value = '4';
                     }
-                    else if(j==1)
+                    else if (j == 1)
                     {
                         *u8_a_value = '5';
                     }
-                    else if(j==2)
+                    else if (j == 2)
                     {
                         *u8_a_value = '6';
                     }
@@ -102,15 +115,15 @@ KEYPAD_readError KEYPAD_read(uint8_t *u8_a_value)
 
                 else if (i == 2)
                 {
-                    if(j == 0)
+                    if (j == 0)
                     {
                         *u8_a_value = '7';
                     }
-                    else if(j==1)
+                    else if (j == 1)
                     {
                         *u8_a_value = '8';
                     }
-                    else if(j==2)
+                    else if (j == 2)
                     {
                         *u8_a_value = '9';
                     }
@@ -118,24 +131,24 @@ KEYPAD_readError KEYPAD_read(uint8_t *u8_a_value)
 
                 else if (i == 3)
                 {
-                    if(j == 0)
+                    if (j == 0)
                     {
                         *u8_a_value = '*';
                     }
-                    else if(j==1)
+                    else if (j == 1)
                     {
                         *u8_a_value = '0';
                     }
-                    else if(j==2)
+                    else if (j == 2)
                     {
                         *u8_a_value = '#';
                     }
                 }
-			    DIO_write(u8_l_Rows[i],ROW_PORT_DATA,HIGH);	                
-				return KEYPAD_readSuccess;
-            }  
+                DIO_toggle(u8_l_Rows[i], ROW_PORT_DATA);
+                return KEYPAD_readSuccess;
+            }
         }
-	DIO_write(u8_l_Rows[i],ROW_PORT_DATA,HIGH);
-	}
+        DIO_toggle(u8_l_Rows[i], ROW_PORT_DATA);
+    }
     return KEYPAD_readFail;
 }
--- main.c
+++ main.c
@@ -14,73 +14,72 @@
 // uint8_t u8_g_bell_1[8] = { 0x04, 0x0E, 0x0E, 0x0E, 0x1F, 0x00, 0x02, 0x00};
 // uint8_t u8_g_bell_2[8] = { 0x04, 0x0E, 0x0E, 0x0E, 0x1F, 0x01, 0x00, 0x08};
 // uint8_t u8_g_bell_3[8] = { 0x04, 0x0E, 0x0E, 0x0E, 0x1F, 0x00, 0x08, 0x00};
-// uint8_t u8_g_bell_4[8] = { 0x04, 0x0E, 0x0E, 0x0E, 0x1F, 0x10, 0x00, 0x08};			
+// uint8_t u8_g_bell_4[8] = { 0x04, 0x0E, 0x0E, 0x0E, 0x1F, 0x10, 0x00, 0x08};
 
 int main(void)
 {
-	uint8_t value;
-	uint8_t count = 0;
-	uint8_t index = 0;
+    uint8_t val[] = "HELLO!";
+    uint8_t value;
+    uint8_t count = 0;
+    uint8_t index = 0;
     /* Replace with your application code */
- 	//uint8_t val[] = "Hi! Omar";
-	LCD_init();
-	KEYPAD_init();
-// 	LCD_createCustomCharacter(u8_g_bell_0,0);
-// 	LCD_createCustomCharacter(u8_g_bell_1,1);
-// 	LCD_createCustomCharacter(u8_g_bell_2,2);
-// 	LCD_createCustomCharacter(u8_g_bell_3,3);
-// 	LCD_createCustomCharacter(u8_g_bell_4,4);
-		
-	while(1)
-	{
-		KEYPAD_read(&value);
+    // uint8_t val[] = "Hi! Omar";
+    LCD_init();
+    KEYPAD_init();
+    LCD_sendString(val);
+    // 	LCD_createCustomCharacter(u8_g_bell_0,0);
+    // 	LCD_createCustomCharacter(u8_g_bell_1,1);
+    // 	LCD_createCustomCharacter(u8_g_bell_2,2);
+    // 	LCD_createCustomCharacter(u8_g_bell_3,3);
+    // 	LCD_createCustomCharacter(u8_g_bell_4,4);
+    TIMER0_delay(700);
+    LCD_sendCommand(Clear_Screen);
+    while (1)
+    {
+        KEYPAD_read(&value);
+        if (((value >= 48) && (value < 58)) || (value == '*') || (value == '#'))
+        {
+            if (index == 0 && count == 16)
+            {
+                index = 1;
+                count = 0;
+                LCD_goTo(index, count);
+            }
 
- 		if(value != 1 )
- 		{
-			 if(index == 0 && count == 16)
-			 {
-				 index = 1;
-				 count = 0;
-				 LCD_goTo(index,count);
-			 }
-			 
-			if(index == 1 && count == 16)
-			{
-				index = 0;
-				count = 0;
-				LCD_sendCommand(Clear_Screen);
-				LCD_goTo(index,count);
-			}
-			LCD_sendChar(value);
-			TIMER0_delay(200);
-			count++;	
-		}
-		value = 1;
-	}
-// 	LCD_goTo(0,8);	
-// 	LCD_sendChar(0);
-// 	TIMER0_delay(50);
-// 	LCD_goTo(0,8);
-// 	LCD_sendChar(1);
-// 	TIMER0_delay(50);
-// 	LCD_goTo(0,8);
-// 	LCD_sendChar(2);
-// 	TIMER0_delay(50);
-// 	LCD_goTo(0,8);
-// 	LCD_sendChar(1);
-// 	TIMER0_delay(50);
-// 	LCD_goTo(0,8);
-// 	LCD_sendChar(0);
-// 	TIMER0_delay(50);
-// 	LCD_goTo(0,8);
-// 	LCD_sendChar(3);
-// 	TIMER0_delay(50);
-// 	LCD_goTo(0,8);
-// 	LCD_sendChar(4);
-// 	TIMER0_delay(50);
-// 	LCD_goTo(0,8);
-// 	LCD_sendChar(3);
-// 	TIMER0_delay(50);
-	
-
+            if (index == 1 && count == 16)
+            {
+                index = 0;
+                count = 0;
+                LCD_sendCommand(Clear_Screen);
+                LCD_goTo(index, count);
+            }
+            LCD_sendChar(value);
+            TIMER0_delay(200);
+            count++;
+        }
+    }
+    // 	LCD_goTo(0,8);
+    // 	LCD_sendChar(0);
+    // 	TIMER0_delay(50);
+    // 	LCD_goTo(0,8);
+    // 	LCD_sendChar(1);
+    // 	TIMER0_delay(50);
+    // 	LCD_goTo(0,8);
+    // 	LCD_sendChar(2);
+    // 	TIMER0_delay(50);
+    // 	LCD_goTo(0,8);
+    // 	LCD_sendChar(1);
+    // 	TIMER0_delay(50);
+    // 	LCD_goTo(0,8);
+    // 	LCD_sendChar(0);
+    // 	TIMER0_delay(50);
+    // 	LCD_goTo(0,8);
+    // 	LCD_sendChar(3);
+    // 	TIMER0_delay(50);
+    // 	LCD_goTo(0,8);
+    // 	LCD_sendChar(4);
+    // 	TIMER0_delay(50);
+    // 	LCD_goTo(0,8);
+    // 	LCD_sendChar(3);
+    // 	TIMER0_delay(50);
 }
